d122fd2804f4e3b9464eeae073068116ddd0d86a
Odysa <piensengxv@gmail.com>
2021-01-03 22:01:14 +0800

    feat: add env to calc

--- JavaScript/eval.js
+++ JavaScript/eval.js
@@ -1,7 +1,16 @@
-function interperator(form) {
-  if (!(form instanceof Array)) return form;
-  const v1 = interperator(form[1]);
-  const v2 = interperator(form[2]);
+const defaultEnv = { next: null, x: 3 };
+function lookUp(key, env) {
+  if (!env) return null;
+  return env[key] ? env[key] : lookUp(key, env.next);
+}
+function extEnv(inner, outer) {
+  inner.next = outer;
+}
+function interperator(form, env) {
+  if (typeof form === "number") return form;
+  if (typeof form === "string") return lookUp(form, env);
+  const v1 = interperator(form[1], env);
+  const v2 = interperator(form[2], env);
   switch (form[0]) {
     case "+":
       return v1 + v2;
@@ -17,5 +26,5 @@ function interperator(form) {
       throw new Error("Invalid operation " + form[0]);
   }
 }
-const form = ["+", ["-", ["*", 3, 4], 2], ["+", 1, 2]];
-console.log(interperator(form));
+const form = ["+", ["-", ["*", "x", 4], 2], ["+", 1, 2]];
+console.log(interperator(form, defaultEnv));
