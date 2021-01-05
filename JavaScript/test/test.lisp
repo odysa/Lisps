7f5bc8b9aec9dec780b90dbdbac3e5580a0805d5
odysa <piensengxv@gmail.com>
2021-01-05 21:48:04 +0800

    add more tests

--- JavaScript/test/test.lisp
+++ JavaScript/test/test.lisp
@@ -1,1 +1,8 @@
-(print (* 1 2 (+ 3 4) (* 1 2) (/ 4 2)))
+(define x 1)
+(print (* x 2 (+ 3 4) (* 1 2) (/ 4 2)))
+(print x)
+
+(define (add a b) (+ a b))
+(print (add 1 2))
+
+(print (let ((x 2)(y 2)) (+ x y)))
