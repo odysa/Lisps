6904cbc1cb5991467dc253ca5b572d92e78ba618
odysa <piensengxv@gmail.com>
2021-01-19 11:31:42 +0800

    test env

--- Go/environment.go
+++ Go/environment.go
@@ -1,26 +1,35 @@
 package Scheme
 
+type DefinitionMap map[string]interface{}
+
 type Env interface {
 	Get(key string) interface{}
 	Set(key string, val interface{})
 }
 
 type NestEnvironment struct {
-	env    map[string]interface{}
-	parent *Env
+	env    DefinitionMap
+	parent Env
+}
+
+func NewNestEnvironment(parent Env) *NestEnvironment {
+	return &NestEnvironment{
+		env:    make(DefinitionMap),
+		parent: parent,
+	}
 }
 
 func (n *NestEnvironment) Get(key string) interface{} {
 	val, ok := n.env[key]
 	if !ok {
-		val = (*n.parent).Get(key)
+		val = n.parent.Get(key)
 	}
 	return val
 }
 
 func (n *NestEnvironment) Set(key string, val interface{}) {
 	if n.env == nil {
-		n.env = make(map[string]interface{})
+		n.env = make(DefinitionMap)
 	}
 	n.env[key] = val
 }
---
+++ Go/environment_test.go
@@ -0,0 +1,24 @@
+package Scheme
+
+import "testing"
+
+func TestNestEnvironmentSet_Get(t *testing.T) {
+	env1 := NewNestEnvironment(nil)
+	env1.Set("a", func() int { return 1 })
+	if env1.Get("a").(func() int)() != 1 {
+		t.Errorf("should be 1, got %v", env1.Get("a").(func() int)())
+	}
+}
+
+func TestNestEnvironment(t *testing.T) {
+	env1 := NewNestEnvironment(nil)
+	env1.Set("a", func() int { return 1 })
+	env2 := NewNestEnvironment(env1)
+	if env1.Get("a").(func() int)() != 1 {
+		t.Errorf("should be 1, got %v", env1.Get("a").(func() int)())
+	}
+	env2.Set("a", func() int { return 2 })
+	if env2.Get("a").(func() int)() != 2 {
+		t.Errorf("should be 2, got %v", env1.Get("a").(func() int)())
+	}
+}
